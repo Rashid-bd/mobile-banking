7cf6eda6546e143ea0ca566ce98f7ccd8780580a
Rashid-bd <panan.cn123.com>
2026-04-29 04:48:15 +0600

    add login button functionality

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+// login button functionality
+
+document.getElementById("login-btn").addEventListener("click", function(event){
+  event.preventDefault();
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+    console.log("login successful");
+  }
+  else{
+    alert("invalid credential");
+  }
+})
